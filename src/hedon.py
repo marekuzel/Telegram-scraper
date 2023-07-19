c3fc45bec8d16201270a5aa51530f876a0d686ca
marekuzel <marekuzel1@gmail.com>
2023-07-19 11:32:32 +0200

    Implemented channel list
    Script now checks for channels.txt where is the list of the channels
    that are to be scrapped

--- src/hedon.py
+++ src/hedon.py
@@ -28,11 +28,40 @@ with TelegramClient(username, api_id, api_hash) as client:
         limit=100,
         hash=0,
     ))
+    
+try:
+    with open ("channels.txt", "r") as f:
+        listOfChannels = f.readlines()
+        listOfChannels = [x.strip() for x in listOfChannels]
+except FileNotFoundError:
+    print ("No channel list exists. Do you want to generate one? (y/n)")
+    if input() == "n":
+        exit()
+    with open("channels.txt", "w") as f:
+        for chat in result.chats:
+            f.write(chat.title + "\n")
+except IOError as e:
+    print(f"An error occurred: {str(e)}")
+
 d = {}
+
+for chat in result.chats:
+    if str(chat.title) in listOfChannels:
+        print (chat.title)
+    else:
+        print (f"{chat.title} not selected")
+print ("-------------------------------------------------------------------------------")
+print ("These are the channels picked up by the script. Do you want to continue? (y/n)")
+if input() == "n":
+    exit()
+    
 with TelegramClient(username, api_id, api_hash) as client:
     for chat in result.chats:
         if chat.megagroup:
             print (f"{chat.title} is not a channel. Skipping...")
+            continue
+        elif chat.title not in listOfChannels:
+            print (f"{chat.title} is not in the list of channels. Skipping...")
             continue
         max_messages = 100
         print (f"{chat.title} in progress...")
